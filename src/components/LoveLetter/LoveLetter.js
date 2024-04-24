477b70e4d8dc8a842c7b9508cff01b8b7d57f09e
Srikanth <dilippasagadugula@gmail.com>
2024-04-24 23:41:57 +0530

    Sample

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './123.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+      Hey buddy🖤,<br />
+      Happy Birthday! Just wanted to drop a quick note to say that even though time has passed, the memories we shared are still fresh in my mind. I hope this special day brings you all the happiness and success you deserve. Even though we've gone our separate ways, you'll always hold a special place in my heart.<br />
+       
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
